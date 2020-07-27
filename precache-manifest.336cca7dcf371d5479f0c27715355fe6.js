95fe10ded1bcb88e13b7b91810c9f4f0278d0c56
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2020-07-28 02:12:25 +0900

    Updates

---
+++ precache-manifest.336cca7dcf371d5479f0c27715355fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0380ad5f34ac448763dcfa5274c26572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7c0e8bf5cd88b1c25bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ad4d80490718eb830dd",
+    "url": "/static/js/2.20132f25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20132f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c0e8bf5cd88b1c25bb",
+    "url": "/static/js/main.ae4fd0b1.chunk.js"
+  },
+  {
+    "revision": "beadf0ffa15a6cb25ba3",
+    "url": "/static/js/runtime-main.5c06e18d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
